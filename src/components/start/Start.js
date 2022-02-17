e3bc2d225053fd9be37caea85bff3908c1b18310
Mohammed Modather <mohammedmodather144@gmail.com>
2022-02-17 04:52:47 +0200

    bulid structure components start using html

--- src/components/start/Start.js
+++ src/components/start/Start.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from 'react';
 
 export default function Start() {
   return (
-    <div>Start</div>
-  )
+    <div className='start'>
+      <div className='start__header'>
+        <h2 className='start__header-title'>x o</h2>
+      </div>
+      <div className='card shadow-gray'>
+        <h3 className='text-lg'>Pick Player 1's mark</h3>
+        <div className='start__players'>
+          <span className='start__players--active'>x</span>
+          <span>o</span>
+        </div>
+        <p className='text-light'>Remember: x goes first</p>
+      </div>
+      <div className='start__btn-group'>
+        <button className='btn btn-yellow'>New game (vs CPU)</button>
+        <button className='btn btn-blue'>New game (vs Player)</button>
+      </div>
+    </div>
+  );
 }
