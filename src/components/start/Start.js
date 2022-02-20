7bebf0420909fb880be10b2443e2601a01cdd552
Mohammed Modather <mohammedmodather144@gmail.com>
2022-02-20 16:14:58 +0200

    Finished design as section start and add icon needed as this section

--- src/components/start/Start.js
+++ src/components/start/Start.js
@@ -1,16 +1,25 @@
 import React from 'react';
+import OIcon from '../icons/Oicon';
+import XIcon from '../icons/Xicon';
 
 export default function Start() {
   return (
     <div className='start'>
       <div className='start__header'>
-        <h2 className='start__header-title'>x o</h2>
+        <h2 className='start__header-title'>
+          <XIcon />
+          <OIcon />
+        </h2>
       </div>
       <div className='card shadow-gray'>
         <h3 className='text-lg'>Pick Player 1's mark</h3>
         <div className='start__players'>
-          <span className='start__players--active'>x</span>
-          <span>o</span>
+          <span className='start__players--active'>
+            <XIcon color='dark' />
+          </span>
+          <span>
+            <OIcon color='light' />
+          </span>
         </div>
         <p className='text-light'>Remember: x goes first</p>
       </div>
